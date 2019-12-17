54dce88b45b6082a671267a4fdb12ae62b9d568a
Raheem Idowu <abdurraheemidowu@gmail.com>
2019-12-17 16:10:28 +0000

    Added tooltips to toolbar icons

--- application/MainWindowRB.cpp
+++ application/MainWindowRB.cpp
@@ -30,23 +30,26 @@ MainWindowRB::MainWindowRB(void)
     fAutohiding(false)
 {
 	SetFlags(Flags() | B_FULL_POINTER_HISTORY);
-	
+
 	unique_ptr<BMessage> navigationMessage(new BMessage(M_PAGENUMBER));
 	fPageNavigationView = new PageNavigationView(move(navigationMessage));
 
 	ImageButton* printButton = new ImageButton("print", new BMessage(MSG_PRINT_DOCUMENT), 0.3, 1);
     printButton->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     printButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
-	
+    printButton->SetToolTip("Print document");
+
 	fOpenFileButton = new ImageButton("open_document",
         new BMessage(MSG_OPEN_FILE_PANEL), 0.3, 1);
     fOpenFileButton->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     fOpenFileButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
-    
+    fOpenFileButton->SetToolTip("Open new document");
+
     fQuitButton = new ImageButton("quit", new BMessage(MSG_APP_QUIT), 0.3, 1);
     fQuitButton->SetExplicitMinSize(BSize(fMaxHeight, 0));
     fQuitButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
-    
+    fQuitButton->SetToolTip("Close document");
+
     // Home Tab
     BGroupLayout* homeLayout = BGroupLayoutBuilder(B_HORIZONTAL, 0)
         .SetInsets(2, 2, 2, 2)
@@ -62,34 +65,46 @@ MainWindowRB::MainWindowRB(void)
         new BMessage(MSG_FULLSCREEN) , 0.3, 1);
     fullscreenButton->SetExplicitMinSize(BSize(fMaxHeight, 0));
     fullscreenButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
+    fullscreenButton->SetToolTip("Fullscreen");
+
     ImageButton* fitPageHeight = new ImageButton("fit_page_height",
         new BMessage(MSG_FIT_PAGE_HEIGHT), 0.3, 1);
     fitPageHeight->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     fitPageHeight->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
+    fitPageHeight->SetToolTip("Fit document vertically");
+
     ImageButton* fitPageWidth = new ImageButton("fit_page_width",
         new BMessage(MSG_FIT_PAGE_WIDTH), 0.3, 1);
     fitPageWidth->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     fitPageWidth->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
-
+    fitPageWidth->SetToolTip("Fit document horizonatally");
 
 	ImageButton* zoomOriginal = new ImageButton("zoom_original", new BMessage(MSG_NO_ZOOM), 0.3, 1);
     zoomOriginal->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     zoomOriginal->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
-    
+    zoomOriginal->SetToolTip("Reset zoom");
+
     ImageButton* zoomOut = new ImageButton("zoom_out",
         new BMessage(MSG_ZOOM_OUT), 0.3, 1);
     zoomOut->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     zoomOut->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
+    zoomOut->SetToolTip("Zoom out");
+
     ImageButton* zoomIn = new ImageButton("zoom_in",
         new BMessage(MSG_ZOOM_IN), 0.3, 1);
     zoomIn->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     zoomIn->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
+    zoomIn->SetToolTip("Zoom in");
+
     ImageButton* rotateLeftButton = new ImageButton("rotate_left", nullptr, 0.3, 1);
     rotateLeftButton->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     rotateLeftButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
+    rotateLeftButton->SetToolTip("Rotate left");
+
     ImageButton* rotateRightButton = new ImageButton("rotate_right", nullptr, 0.3, 1);
     rotateRightButton->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     rotateRightButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
+    rotateLeftButton->SetToolTip("Rotate right");
 
 
     // View Tab
@@ -121,11 +136,12 @@ MainWindowRB::MainWindowRB(void)
         .AddGlue(0)
         .RootLayout()
     ;
-    
-    
+
+
     ImageButton* setupPrinterButton = new ImageButton("printer", new BMessage(MSG_SETUP_PRINTER), 0.3, 1);
     setupPrinterButton->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     setupPrinterButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
+    setupPrinterButton->SetToolTip("Set up printer");
 
     // Configure Tab
     BGroupLayout* configureLayout = BGroupLayoutBuilder(B_HORIZONTAL, 0)
@@ -133,18 +149,20 @@ MainWindowRB::MainWindowRB(void)
         .AddGlue(0)
     	.RootLayout()
     ;
-    
+
     ImageButton* helpButton = new ImageButton("help2",
     	new BMessage(MSG_HELP), 0.3, 1);
    	helpButton->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     helpButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
-    
+    helpButton->SetToolTip("Help");
+
     ImageButton* supportButton = new ImageButton("support",
     	new BMessage(MSG_SUPPORT), 0.3, 1);
    	supportButton->SetExplicitMinSize(BSize(fMaxHeight, fMaxHeight));
     supportButton->SetExplicitMaxSize(BSize(fMaxHeight, fMaxHeight));
-    
-    
+    supportButton->SetToolTip("Support Documentviewer");
+
+
     // Configure Tab
     BGroupLayout* helpLayout = BGroupLayoutBuilder(B_HORIZONTAL, 0)
         .Add(helpButton)
@@ -237,14 +255,14 @@ MainWindowRB::MessageReceived(BMessage* message)
             message->FindInt32("info", &value);
             ++value;
 			fPageNavigationView->SetValue(value);
-			
+
             break;
         }
-        
+
         case M_PAGENUMBER:
         	_GoToPage(fPageNavigationView->Value() - 1);
         	break;
-        	
+
         default:
             BGroupView::MessageReceived(message);
     }
